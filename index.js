8a9f8e2936413b61792fa9d12926a58e7b9961a6
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-03-11 21:34:15 -0800

    oop

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+// Class
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
