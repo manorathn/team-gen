14e1862a9283ad6ece713f12b42b3e4845e2bd6c
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-03-13 19:45:16 -0800

    update

---
+++ _test_/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/Employee");
+
+describe("getName", () => {
+    it("Returns name", () => {
+        const name = 'Tony';
+        const data = new Employee(name);
+        expect(data.getName()).toBe(name);
+    });
+});
+
+describe("getId", () => {
+    it("Returns id", () => {
+        const id = 1;
+        const data = new Employee('Tony', id);
+        expect(data.getId()).toBe(id);
+    });
+});
+
+describe("getEmail", () => {
+    it("Returns email", () => {
+        const email = 'stark@mail.com';
+        const data = new Employee('Tony', 1, email);
+        expect(data.getEmail()).toBe(email);
+    });
+});
+
+describe("getRole", () => {
+    it("Returns role", () => {
+        const role = "Employee";
+        const data = new Employee('Tony', 1, 'stark@mail.com');
+        expect(data.getRole()).toBe(role);
+    });
+});
--- index.js
+++ index.js
@@ -5,9 +5,77 @@ const util = require('util');
 
 // Class
 const Engineer = require('./lib/Engineer');
-const Manager = require('./lib/Manager');
+const manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
 // create writeFile function using promises instead of a callback function
 const writeFileAsync = util.promisify(fs.writeFile);
 
+const managerQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your manager's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your manager's email address?"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is your manager's office number?"
+    }
+];
+
+const engineerQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your engineer's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your engineer's email address?"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is your engineer's GitHub username?"
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your intern's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your intern's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your intern's email address?"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What university does your intern attend?"
+    }
+];
